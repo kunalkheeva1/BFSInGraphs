9ddbf3004160d5e7441a78f5e9bb0d9a78a677f5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-30 22:20:35 -0700

    added the comments for the better understanding, please read it before doing the problem, its so easy one

--- src/BFSInGraphs.java
+++ src/BFSInGraphs.java
@@ -4,18 +4,29 @@ import java.util.Queue;
 
 public class BFSInGraphs {
 
+
+    //bsf algorithm, in which i am storing the vertices in a list to be returned
+    // first, i will keep the record of the visited elements, 1 is for the visited and 0 is for not
+    // queue will be use to store the current node and add the next connections accordingly
     public static ArrayList<Integer> bFSInGraphs(int V, ArrayList<ArrayList<Integer>> adj){
         ArrayList<Integer> ans = new ArrayList<>();
 
+        // initially mark every position unvisited
         int [] visited = new int[V];
         for(int i =0; i<V; i++){
             visited[i] = 0;
         }
 
+        // start with adding 0, and mark 0 as visited in visited array
         Queue<Integer> q = new LinkedList<>();
         q.add(0);
         visited[0] =1;
 
+
+        // until the queue gets emptied, remove the current and add it in ans,
+        // then run a loop for the connections of the current and check if its visited or not
+        // if not mark it visited and add it in queue and repeat the process
+        // and at the end just return the answer
         while(!q.isEmpty()){
             int current = q.remove();
             ans.add(current);
