3a7e3e6c9db6dd94fe28895ae8d9aa9f5a2cd285
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-30 22:09:20 -0700

    coded a method to represent the bsf which is basically so easy, I will explain it in comments so read the things after next commit so carefully

--- src/BFSInGraphs.java
+++ src/BFSInGraphs.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFSInGraphs {
 
-    
+    public static ArrayList<Integer> bFSInGraphs(int V, ArrayList<ArrayList<Integer>> adj){
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int [] visited = new int[V];
+        for(int i =0; i<V; i++){
+            visited[i] = 0;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        visited[0] =1;
+
+        while(!q.isEmpty()){
+            int current = q.remove();
+            ans.add(current);
+            for(int i =0; i<adj.get(current).size(); i++){
+                int nextConnection = adj.get(current).get(i);
+                if(visited[nextConnection] !=1){
+                    visited[nextConnection]= 1;
+                    q.add(nextConnection);
+                }
+            }
+        }return ans;
+    }
 
 
 
